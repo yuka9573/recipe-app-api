2044aba9c938daf84334142490632b48d4728b70
杉原優花 <sugiharayuka@sugiharayukanoMacBook-Air.local>
2023-07-06 23:03:05 +0900

    fixed models.py

--- app/core/models.py
+++ app/core/models.py
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    name = models.BooleanField(max_length=255)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,11 +11,11 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
-        password = 'testpassword123'
+        password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
